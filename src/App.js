deb83a6a59e642f2fd6f5ca482c3d6c9fd2becb7
AlexzanderManning <deonte94@icloud.com>
2019-11-22 14:29:28 -0500

    added redux, and redirecting user home if signed in

--- src/App.js
+++ src/App.js
@@ -1,53 +1,39 @@
 import React from 'react';
 import "./App.css";
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from './pages/shop-page/shop-page.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component';
 import { auth , createUserProfileDocument} from './firebase/firebase.utils';
-//
+import {connect } from 'react-redux';
+import {setCurrentUser} from './redux/user/user.actions';
 
 
 
-//exact - controls if the url must match exactly to render that component
-//path - is the path of the url
-// component - is the component it will render.
-//Switch - will only render a single component if a url path matches. 
-// /hats will not render because it mathces "/" first.
-//will only render one component.
-//As long as the route matches onnly that component will render.
+
+
 
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    };
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
           });
-
-          console.log(this.state);
         });
       }
 
-      this.setState({ currentUser: userAuth });
+      setCurrentUser(userAuth);
     });
   }
 
@@ -58,15 +44,36 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUp} />
+          <Route
+            exact
+            path='/signin'
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+                <SignInAndSignUp />
+              )
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom'; //Imports React Router
+import { BrowserRouter } from 'react-router-dom'; //Imports React Router
+import { Provider } from 'react-redux';
+import store  from './redux/store';
 
 
 //App has to be wrapped inside Browser router.
 ReactDOM.render(
-  <BrowserRouter> 
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
